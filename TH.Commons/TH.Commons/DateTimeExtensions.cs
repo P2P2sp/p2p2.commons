f101c35cdbfc28dee0c311ceaa13ab42bb2b10f5
Lech Osiński <osinski.lech@gmail.com>
2017-01-31 18:32:35 +0100

    dodana metoda DateTime.ToStringDatetime2

---
+++ TH.Commons/TH.Commons/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TH.Commons
+{
+    /// <summary>
+    /// Extenstion methods for DateTime type
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Converts datetime to string in datetime2(7) sql data format (with 100ns precision)
+        /// </summary>
+        public static string ToStringDatetime2(this DateTime value)
+        {
+            return value.ToString("yyyy.MM.dd HH:mm:ss.fffffff");
+        }
+    }
+}
