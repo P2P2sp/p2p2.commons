7d015c06e514ea01aaee52954328a35e5eb7dcc3
Lech Osiński <osinski.lech@gmail.com>
2017-02-01 13:16:43 +0100

    dodatkowe metody pomocnicze dla DateTime

--- TH.Commons/TH.Commons/DateTimeExtensions.cs
+++ TH.Commons/TH.Commons/DateTimeExtensions.cs
@@ -14,5 +14,25 @@ namespace TH.Commons
         {
             return value.ToString("yyyy.MM.dd HH:mm:ss.fffffff");
         }
+
+        /// <summary>
+        /// Returns true if both datetimes are equal from Date, through Hours and Minutes, to Seconds (so it allows mismatch from miliseconds forwards)
+        /// </summary>
+        public static bool AreEqualToTheSeconds(DateTime dateTime1, DateTime dateTime2)
+        {
+            return dateTime1.Date == dateTime2.Date
+                && dateTime1.Hour == dateTime2.Hour
+                && dateTime1.Minute == dateTime2.Minute
+                && dateTime1.Second == dateTime2.Second;
+        }
+
+        /// <summary>
+        /// Returns true if both datetimes are equal from Date, through Hours and Minutes, to Seconds (so it allows mismatch from miliseconds forwards)
+        /// </summary>
+        public static bool AreEqualToTheSeconds(DateTime? dateTime1, DateTime? dateTime2)
+        {
+            if (dateTime1 == null || dateTime2 == null) return false;
+            return AreEqualToTheSeconds(dateTime1.Value, dateTime2.Value);
+        }
     }
 }
