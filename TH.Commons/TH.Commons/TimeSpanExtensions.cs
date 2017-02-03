80670444dafbf840a20e722b90d7ae33749be464
Lech Osiński <osinski.lech@gmail.com>
2017-02-03 23:42:32 +0100

    dodatkowa metoda ToReadableStringWholeTime dla TimeSpan

--- TH.Commons/TH.Commons/TimeSpanExtensions.cs
+++ TH.Commons/TH.Commons/TimeSpanExtensions.cs
@@ -19,6 +19,9 @@ namespace TH.Commons
             return $"{span.Days / 365.25:0}";
         }
 
+        /// <summary>
+        /// Displays days if any, and after that hours if not zero, minutes if not zero and/or seconds if not zero. If param is zero displays 0s.
+        /// </summary>
         public static string ToReadableString(this TimeSpan span)
         {
             var formatted = string.Format("{0}{1}{2}{3}",
@@ -26,11 +29,22 @@ namespace TH.Commons
                 span.Duration().Hours > 0 ? $"{span.Hours:0}g " : string.Empty,
                 span.Duration().Minutes > 0 ? $"{span.Minutes:0}min " : string.Empty,
                 span.Duration().Seconds > 0 ? $"{span.Seconds:0}s" : string.Empty);
-
             if (formatted.EndsWith(" ")) formatted = formatted.TrimEnd();
-
             if (string.IsNullOrEmpty(formatted)) formatted = "0s";
+            return formatted;
+        }
 
+        /// <summary>
+        /// Displays days if any, and after that time (hours, minutes and second) if any of its parts are not zero. If param is zero displays 0s.
+        /// </summary>
+        public static string ToReadableStringWholeTime(this TimeSpan span)
+        {
+            var formatted = string.Format("{0}{1}",
+                span.Duration().Days > 0 ? $"{span.Days:0}d. " : string.Empty,
+                span.Duration().Hours > 0 || span.Duration().Minutes > 0 || span.Duration().Seconds > 0 ? 
+                $"{span.Hours:0}:{span.Minutes:0}:{span.Seconds:0}" : string.Empty);
+            if (formatted.EndsWith(" ")) formatted = formatted.TrimEnd();
+            if (string.IsNullOrEmpty(formatted)) formatted = "0s";
             return formatted;
         }
     }
