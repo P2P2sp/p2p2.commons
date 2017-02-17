5c72969c1fda9de38757a4b068a0565d09699894
Lech Osiński <osinski.lech@gmail.com>
2017-02-17 16:28:38 +0100

    int.ToStringHoursAndMinutes

---
+++ TH.Commons/TH.Commons/IntExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace TH.Commons
+{
+    /// <summary>
+    /// Integer extensions
+    /// </summary>
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// Returns full hours and remainder of minutes from given minutes value
+        /// </summary>
+        public static string ToStringHoursAndMinutes(this int minutes)
+        {
+            if (minutes < 60) return $"{minutes}";
+            var hours = minutes / 60;
+            var rest = minutes % 60;
+            return $"{hours}g {rest}m";
+        }
+    }
+}
