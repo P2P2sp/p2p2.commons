d942106fe3ee2a3632fc933ed7e52584b509a1da
Lech Osiński <osinski.lech@gmail.com>
2017-10-31 12:41:54 +0100

    poprawki opisów 2

--- TH.Commons/TH.Commons/DateTimeExtensions.cs
+++ TH.Commons/TH.Commons/DateTimeExtensions.cs
@@ -44,9 +44,8 @@ namespace TH.Commons
         }
 
         /// <summary>
-        /// For given DateTime value, method returns the closest date after which DayOfWeek property equal to given dayOfWeek parameter, 
-        /// or returns given DateTime value, if its DayOfWeek equals given day parameter.
-        /// Calls ".AddDays(1)" method from 0 to 6 times.
+        /// For given DateTime value, method returns the closest date after, which DayOfWeek property equal to given dayOfWeek parameter, 
+        /// or returns given DateTime value, if its DayOfWeek equals given dayOfWeek parameter.
         /// </summary>
         public static DateTime GetDateNextOrCurrent(this DateTime value, DayOfWeek dayOfWeek)
         {
@@ -57,7 +56,7 @@ namespace TH.Commons
         }
 
         /// <summary>
-        /// For given DateTime value, method returns the closest date after which DayOfWeek property equal to given dayOfWeek parameter.
+        /// For given DateTime value, method returns the closest date after, which DayOfWeek property equal to given dayOfWeek parameter.
         /// </summary>
         public static DateTime GetDateNext(this DateTime value, DayOfWeek dayOfWeek)
         {
@@ -68,8 +67,8 @@ namespace TH.Commons
         }
 
         /// <summary>
-        /// For given DateTime value, method returns the closest date before which DayOfWeek property equal to given dayOfWeek parameter, 
-        /// or returns given DateTime value, if its DayOfWeek equals given day parameter.
+        /// For given DateTime value, method returns the closest date before, which DayOfWeek property equal to given dayOfWeek parameter, 
+        /// or returns given DateTime value, if its DayOfWeek equals given dayOfWeek parameter.
         /// </summary>
         public static DateTime GetDatePreviousOrCurrent(this DateTime value, DayOfWeek dayOfWeek)
         {
@@ -80,7 +79,7 @@ namespace TH.Commons
         }
 
         /// <summary>
-        /// For given DateTime value, method returns the closest date before which DayOfWeek property equal to given dayOfWeek parameter.
+        /// For given DateTime value, method returns the closest date before, which DayOfWeek property equal to given dayOfWeek parameter.
         /// </summary>
         public static DateTime GetDatePrevious(this DateTime value, DayOfWeek dayOfWeek)
         {
