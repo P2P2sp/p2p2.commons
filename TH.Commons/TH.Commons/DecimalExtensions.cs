812e5296d9a328a00d149c59cb52bc27f736deaa
Lech Osiński <osinski.lech@gmail.com>
2017-11-16 12:13:29 +0100

    poprawka (Truncate nachodziło na statyczną metodę decimal.Truncate)

--- TH.Commons/TH.Commons/DecimalExtensions.cs
+++ TH.Commons/TH.Commons/DecimalExtensions.cs
@@ -9,7 +9,7 @@
         /// Returns decimal rounded down (or "towards zero") to given decimals count. 
         /// For 1,279 and 2 decimals, returns 1,27. For -2,7654 and 1 decimals returns -2,7. And so on.
         /// </summary>
-        public static decimal Truncate(this decimal d, byte decimals)
+        public static decimal TruncateToDecimals(this decimal d, byte decimals)
         {
             var r = decimal.Round(d, decimals);
 
